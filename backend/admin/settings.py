9111fe457398cf3262640712773adf1634a67305
patrick Muniz <patrick.muniz@defensoria.ro.def.br>
2023-01-13 16:44:51 -0400

    inclui env de SECRET_KEY

--- backend/admin/settings.py
+++ backend/admin/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-29p12#o6)u4vy-ehl0*klv=2*#51rsiat#r=l#bjsov1@_0mh("
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
